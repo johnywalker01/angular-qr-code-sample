98563adb87b6a2ea3ddb2e914e5404a04b615f6a
milton <milton.t@quest-global.com>
2023-06-06 16:45:53 +0530

    removed cordova deps

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-const bootstrap = () => {
-  platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
-};
-if (typeof (window as any)["cordova"] !== "undefined") {
-  document.addEventListener(
-    "deviceready",
-    () => {
-      bootstrap();
-    },
-    false
-  );
-} else {
-  bootstrap();
-}
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
 
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log((navigator as any)?.camera);
-}
